e83a961bb9a372dfdd8487daa865dcb2f8a38ecd
Jastagar <jastagar1581.be20@chitkara.edu.in>
2022-07-22 09:38:45 +0530

    removed Mongo

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const axios = require("axios")
-const mongo = require('mongoose')
+const fs = require("fs")
 
-console.log(process.env.DATABASE_URL)
-
-mongo.connect(process.env.DATABASE_URL).then((res)=>{
-    const something = res.Collection("3rd-year-schdule")
-    console.log(something);
-    console.log("Connected");
-}).catch((err)=>{
-    console.log("Error",err);
-})
-
-
+const dataContent = fs.readFileSync("./data.json").toString()
+const parsedData = JSON.parse(dataContent)
 
 app.set('view engine',"ejs")
 app.use(express.static("public"))
 
-// const {readFileSync} = require('fs');
-
-var dataArr = []
 
 app.get("/schdule",(req,res)=>{
-    res.render('schdule',{dataArr})
+    res.render('schdule',{dataArr:parsedData})
 })
 app.get("/",(req,res)=>{
     res.render("index")
 })
 
 
-app.listen(3000,()=>{
-    console.log("Server running at 3000")
-})
+app.listen(process.env.PORT)
